15ff692ee83d042749034de066efe36f75a9e5eb
DarkestSoul1992 (John Wolfe) <johnwolfe001@hotmail.co.uk>
2024-03-07 09:37:07 +0000

    Update Parser.php

--- src/Parser.php
+++ src/Parser.php
@@ -19,14 +19,14 @@
 			for ($i = 0; $i < count($array); ++$i) {
 				# Check for the Markdown Header level 1, remove it and add the h1 opening and closing tags for HTML...
 				if (preg_match($this->FindMDHeader1, $array[$i]) == 1) {
-					preg_replace($this->FindMDHeader1, '<h1>', $array[$i]);
+					$array[$i] = preg_replace($this->FindMDHeader1, '<h1>', $array[$i]);
 					# Need to remove the Markdown newline character so it isn't processed later as we are adding our own manually here...
-					preg_replace($this->FindMDNewline, '', $array[$i]);
+					$array[$i] = preg_replace($this->FindMDNewline, '', $array[$i]);
 					$array[$i] .= '</h1><br>';
 					echo $array[$i];
 				}
 				# Next to split the string by the markdown double space newline and append the HTML newline to the end of each of the strings in the resulting array...
-				preg_replace($this->FindMDNewline, '<br>', $array[$i]);
+				$array[$i] = preg_replace($this->FindMDNewline, '<br>', $array[$i]);
 			}
 			return $array;
 		}
