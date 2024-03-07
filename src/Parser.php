39da05adfeb686b44f91e495b8cf1cb6daac7b60
DarkestSoul1992 (John Wolfe) <johnwolfe001@hotmail.co.uk>
2024-03-07 06:37:42 +0000

    Update Parser.php

--- src/Parser.php
+++ src/Parser.php
@@ -15,9 +15,8 @@
 		# Returns the HTML formatted array of lines contained in the $HtmlFormattedMarkdown array...
 		function Parse() {
 			# First to split the string by the markdown double space newline and append the HTML newline to the end of each of the strings in the resulting array...
-			$string = preg_replace($this->FindMDNewline, '<br>\n', $this->OriginalFileContent);
-			echo $string . '<br>';
-			$array = explode('\n', $string);
+			$string = preg_replace($this->FindMDNewline, '<br>\\n', $this->OriginalFileContent);
+			$array = explode('\\n', $string);
 			# Check for the Markdown Header level 1, remove it and add the h1 opening and closing tags for HTML...
 			foreach ($array as $line) {
 				if (preg_match('/#/', $line)) {
