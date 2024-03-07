5fe96344487834e44be8e880572f3820f53698b4
DarkestSoul1992 (John Wolfe) <johnwolfe001@hotmail.co.uk>
2024-03-07 04:36:22 +0000

    Update Parser.php

--- src/Parser.php
+++ src/Parser.php
@@ -2,7 +2,7 @@
 	namespace WolfNet_Computing\MD_Reader;
 
 	class Parser {
-		private $FindMDNewline = "/\s{2}/";
+		private $FindMDNewline = "/\s{2, }/";
 		private $OriginalFileContent;
 		private $OutputFormat;
 
@@ -16,7 +16,8 @@
 		# Returns the HTML formatted array of lines contained in the $HtmlFormattedMarkdown array.
 		function Parse() {
 			# First to split the string by the markdown double space newline and append the HTML newline to the end of each of the strings in the resulting array.
-			$array = preg_split($this->FindMDNewline, $this->OriginalFileContent);
+			$string = preg_replace($this->FindMDNewline, '<br>\\n', $this->OriginalFileContent);
+			$array = preg_split('/\\n/', $string);
 			for ($i = 0; $i < count($array); ++$i) {
 				$array[$i] .= '<br>';
 			}
