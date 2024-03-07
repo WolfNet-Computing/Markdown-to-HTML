e0100fb127473650530f58a0a9e568abc458528b
DarkestSoul1992 (John Wolfe) <johnwolfe001@hotmail.co.uk>
2024-03-07 05:02:55 +0000

    Update Parser.php

--- src/Parser.php
+++ src/Parser.php
@@ -16,7 +16,7 @@
 		function Parse() {
 			# First to split the string by the markdown double space newline and append the HTML newline to the end of each of the strings in the resulting array...
 			$string = preg_replace($this->FindMDNewline, '<br>\\n', $this->OriginalFileContent);
-			$array = preg_split('/\n/', $string);
+			$array = explode('\\n', $string);
 			# More processing to go here...
 			return $array;
 		}
