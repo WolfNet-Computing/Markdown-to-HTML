a236037f4b0dc2116ba33fa3e05fd16f90d0cf49
DarkestSoul1992 (John Wolfe) <johnwolfe001@hotmail.co.uk>
2024-03-07 04:48:33 +0000

    Update Parser.php

--- src/Parser.php
+++ src/Parser.php
@@ -16,8 +16,8 @@
 		# Returns the HTML formatted array of lines contained in the $HtmlFormattedMarkdown array...
 		function Parse() {
 			# First to split the string by the markdown double space newline and append the HTML newline to the end of each of the strings in the resulting array...
-			$string = preg_replace($this->FindMDNewline, '<br>\\n', $this->OriginalFileContent);
-			$array = preg_split('/\\n/', $string);
+			$string = preg_replace($this->FindMDNewline, '<br>\n', $this->OriginalFileContent);
+			$array = preg_split('/\n/', $string);
 			# More processing to go here...
 			return $array;
 		}
