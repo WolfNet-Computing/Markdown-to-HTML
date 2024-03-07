e3a030200773451b2902ff3e42b5657238e4ef09
DarkestSoul1992 (John Wolfe) <johnwolfe001@hotmail.co.uk>
2024-03-07 03:42:52 +0000

    Update Parser.php

--- src/Parser.php
+++ src/Parser.php
@@ -2,21 +2,22 @@
 	namespace WolfNet_Computing\MD_Reader;
 
 	class Parser {
-		private $OriginalFileContent;
+		private $OriginalFileContent = '';
+		private $OutputType = '';
 
-		function __construct($file, $OutputType) {
-			$mdfile = fopen($file, 'r') or die('Unable to open file!');
-			$this->OriginalFileContent = fread($mdfile,filesize($file));
-			fclose($mdfile);
+		function __construct($OutputType) {
 			if ($OutputType == 'HTML') {
-				return $this->ParseHTML($this->OriginalFileContent);
+				return $this->OutputType = 'HTML';
 			} else {
 				die('Unrecognised output format!');
 			}
 		}
 
 		# Returns the HTML formatted array of lines contained in the $HtmlFormattedMarkdown array.
-		function ParseHTML($HtmlString) {
+		function ParseHTML($file, $HtmlString) {
+			$mdfile = fopen($file, 'r') or die('Unable to open file!');
+			$this->OriginalFileContent = fread($mdfile,filesize($file));
+			fclose($mdfile);
 			# First to split the string by the markdown double space newline and append the HTML newline to the end of each of the strings in the resulting array.
 			$array = explode('  ', $HtmlString);
 			for ($i = 0; $i < count($array); ++$i) {
