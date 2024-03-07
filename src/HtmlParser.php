18952ce04ad1c4f02109f422e64c0f16f7b16f5f
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-03-07 11:36:24 +0000

    Update HtmlParser.php

--- src/HtmlParser.php
+++ src/HtmlParser.php
@@ -2,8 +2,8 @@
 	namespace WolfNet_Computing\MD_Reader;
 
 	class HtmlParser {
-		private $FindMDNewline = '/\u{0020}{2}/u';
-		private $FindMDHeader1 = '/^\u{0023}\u{0020}/u';
+		private $FindMDNewline = '/\u0020{2}/u';
+		private $FindMDHeader1 = '/^\u0023\u0020/u';
 		private $OriginalFileContent;
 
 		function __construct($file) {
