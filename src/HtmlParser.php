104a4951e6998211a02c3562011bd362518b02dd
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-03-09 02:54:52 +0000

    Update HtmlParser.php

--- src/HtmlParser.php
+++ src/HtmlParser.php
@@ -25,9 +25,7 @@
 			$arraybegin = array_slice($array,0,$index - 1);
 			$arraybegin[] = $data;
 			$arrayend = array_slice($array,$index);
-			$lastentry = $array[count($array) - 1];
 			$array = array_merge($arraybegin, $arrayend);
-			$array[] = $lastentry;
 		}
 
 		# Returns the HTML formatted array of lines contained in the $HtmlFormattedMarkdown array...
