901491cc8c4fcc2b00d2bccf9369ff63cd0f8d71
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-03-10 02:09:02 +0000

    Update HtmlParser.php

--- src/HtmlParser.php
+++ src/HtmlParser.php
@@ -49,7 +49,9 @@
 							if (preg_match_all($this->FindMDBoldTextItem, $regexarray1[$j], $regexarray2) > 0) {
 								$finalstr = "";
 								for ($k = 0; $k < count($regexarray2); $k++) {
-									print_r($regexarray2);
+									for ($l = 0; $l < count($regexarray2[$k]); $l++) {
+										echo $regexarray2[$k][$l] . "<br>";
+									}
 									//$boldremoved = explode($regexarray2[$k], $regexarray1[$j]);
 									//if ($k != 0) {
 									//	$finalstr = $finalstr . $boldremoved;
