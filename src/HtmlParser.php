c90afc651e221fc0ab871e5c4972e6bc8b9201ba
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-03-11 19:59:04 +0000

    Update HtmlParser.php

--- src/HtmlParser.php
+++ src/HtmlParser.php
@@ -1,12 +1,6 @@
 <?php
 	namespace MD_Reader;
 
-	class Configuration {
-		function __construct($configarray) {
-			echo gettype($configarray) . "<br>";
-		}
-	}
-
 	class HtmlParser {
 		private $NumberOfPasses = 3;
 
