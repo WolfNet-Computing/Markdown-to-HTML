06cc913906b90869b9e3c291aff88c5e583330da
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-03-11 20:39:45 +0000

    Update Configuration.php

--- src/Configuration.php
+++ src/Configuration.php
@@ -3,12 +3,20 @@
 
 	class Configuration {
 		private $DefaultConfiguration = [
-				'method'			=>	"POST",
-			];
+			'method'	=>	"POST",
+		];
+		public $Configuration;
 
 		function __construct($configarray) {
+			$Configuration = $DefaultConfiguration;
 			foreach ($configarray as $index => $configitem) {
-				echo "\$configarray['" . $index . "'] is: " . $configitem . "and has the type: " . gettype($configitem) . "<br>";
+				echo "\$configarray['" . $index . "'] is: " . $configitem . " and has the type: " . gettype($configitem) . "<br>";
+				foreach ($DefaultConfiguration as $DefaultConfigurationIndex => $DefaultConfigurationItem) {
+					if ($index == $DefaultConfigurationIndex) {
+						echo "Found a match in the default configuration.<br>";
+					}
+					$Configuration[$index] = $configitem;
+				}
 			}
 		}
 	}
