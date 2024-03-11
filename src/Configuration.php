adf1a690c0b23e83b5310c2803f8a5e7d98c8e1f
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-03-11 20:24:51 +0000

    Update Configuration.php

--- src/Configuration.php
+++ src/Configuration.php
@@ -8,8 +8,7 @@
 
 		function __construct($configarray) {
 			foreach ($configarray as $index => $configitem) {
-				echo "\$configarray[\'" . $index . "\'] is: " . $configitem . "<br>";
-				echo "and has the type: " . gettype($configitem) . "<br>";
+				echo "\$configarray['" . $index . "'] is: " . $configitem . "and has the type: " . gettype($configitem) . "<br>";
 			}
 		}
 	}
