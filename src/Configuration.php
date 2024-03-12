6cecf4e6c946b620e6ee3a0656ace2984afa9e32
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-03-12 03:25:43 +0000

    Update Configuration.php

--- src/Configuration.php
+++ src/Configuration.php
@@ -10,13 +10,15 @@
 		function __construct($configarray) {
 			$this->Configuration = $this->DefaultConfiguration;
 			$mdinfo = pathinfo($configarray["doc_file"]);
-			$this->Configuration["doc_file"] = $mdinfo["basename"];
+			$this->Configuration["doc_file"] = preg_replace('/( [\.][\/] )/', "", $mdinfo["basename"]);
 			echo $this->Configuration["doc_file"] . "<br>";
 			$this->Configuration["doc_root"] = $mdinfo["dirname"];
-			$this->Configuration["doc_root"] = preg_replace('/( [\.][\/] )/', "/", $this->Configuration["doc_root"]);
+			$this->Configuration["doc_root"] = preg_replace('/( [\.][\/] )/', "", $this->Configuration["doc_root"]);
 			echo $this->Configuration["doc_root"] . "<br>";
 			foreach ($configarray as $index => $configitem) {
-				$this->Configuration[$index] = $configitem;
+				if ($index != "doc_file" && $index != "doc_root") {
+					$this->Configuration[$index] = $configitem;
+				}
 			}
 			if (array_key_exists("method_var", $this->Configuration) != True) {
 				throw new Exception("class MD_Reader\Configuration doesn't contain a method_var key.");
