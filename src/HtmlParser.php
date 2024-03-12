49d359069db9b7cf2e04acc047c1f96a0aecfde0
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-03-12 03:41:45 +0000

    Update HtmlParser.php

--- src/HtmlParser.php
+++ src/HtmlParser.php
@@ -312,7 +312,7 @@
 							} else {
 								$fileinfo = pathinfo($linkcontent[1]);
 								if ($fileinfo["extension"] == "md") {
-									$FormattedOutput[$i] = preg_replace($this->FindMDLink1, "<a href=\"" . $this->Configuration["doc_handler"] . "?" . $this->Configuration["method_var"] . "=/" . $this->Configuration["doc_root"] . "/" . $fileinfo["basename"] . "\" target=\"_top\">" . $linkcontent[0] . "</a>", $FormattedOutput[$i]);
+									$FormattedOutput[$i] = preg_replace($this->FindMDLink1, "<a href=\"" . $this->Configuration["doc_handler"] . "?" . $this->Configuration["method_var"] . "=" . $this->Configuration["doc_root"] . "/" . $fileinfo["basename"] . "\" target=\"_top\">" . $linkcontent[0] . "</a>", $FormattedOutput[$i]);
 								} else {
 									$FormattedOutput[$i] = preg_replace($this->FindMDLink1, "<a href=\"" . $linkcontent[1] . "\">" . $linkcontent[0] . "</a>", $FormattedOutput[$i]);
 								}
