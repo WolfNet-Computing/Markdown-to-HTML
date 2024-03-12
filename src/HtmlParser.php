f800184c75b5955cc8a13fd615d738b314b04525
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-03-12 00:12:51 +0000

    Update HtmlParser.php

--- src/HtmlParser.php
+++ src/HtmlParser.php
@@ -320,7 +320,7 @@
 								$fileinfo = pathinfo($linkcontent[1]);
 								if ($fileinfo["extension"] == "md") {
 									if ($this->Configuration["method"] == "POST") {
-										$FormattedOutput[$i] =  "<script type=\"text/javascript\">\n\tfunction() {\n\t\tclick" . $i . "(function() {\n\t\t\t$.post(" . $this->Configuration["doc_handler"] . ", {" . $this->Configuration["method_var"] . ": \"" . $linkcontent[1] . "\"});\n\t\t});\n\t});</script>\n<a id=\"Link\" href=\"javascript:void(0);\" onclick=\"click" . $i . "()\">" . $linkcontent[0] . "</a>";
+										$FormattedOutput[$i] =  "<script type=\"text/javascript\">\n\tfunction() {\n\t\tclick" . $i . "(function() {\n\t\t\t$.post(" . $this->Configuration["doc_handler"] . ", {" . $this->Configuration["method_var"] . ": \"" . $linkcontent[1] . "\"});\n\t\t});\n\t});</script>\n<a id=\"Link\" href=\"javascript:void(0)\" onclick=\"click" . $i . "()\">" . $linkcontent[0] . "</a>";
 									} else {
 										$FormattedOutput[$i] = preg_replace($this->FindMDLink1, "<a href=" . $this->Configuration["doc_handler"] . "?" . $this->Configuration["method_var"] . "=" . $linkcontent[1] . ">" . $linkcontent[0] . "</a>", $FormattedOutput[$i]);
 									}
