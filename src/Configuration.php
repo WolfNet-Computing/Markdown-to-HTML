8ba13362ca82486c867ec7e2e8ddd94f1989f825
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-03-12 02:53:17 +0000

    Update Configuration.php

--- src/Configuration.php
+++ src/Configuration.php
@@ -8,8 +8,11 @@
 		public $Configuration;
 
 		function __construct($configarray) {
-			if ($this->Configuration["method"] != "GET") {
-				throw new Exception("class MD_Reader\Configuration contains an invalid method.");
+			$this->Configuration = $this->DefaultConfiguration;
+			$mdinfo = pathinfo($configarray["doc_file"]);
+			$this->Configuration["doc_root"] = $mdinfo["dirname"];
+			foreach ($configarray as $index => $configitem) {
+				$this->Configuration[$index] = $configitem;
 			}
 			if (array_key_exists("method_var", $this->Configuration) != True) {
 				throw new Exception("class MD_Reader\Configuration doesn't contain a method_var key.");
@@ -19,12 +22,6 @@
 			}
 			if (array_key_exists("doc_file", $this->Configuration) != True) {
 				throw new Exception("class MD_Reader\Configuration doesn't contain a doc_file key.");
-			}
-			$this->Configuration = $this->DefaultConfiguration;
-			$mdinfo = pathinfo($configarray["doc_file"]);
-			$this->Configuration["doc_root"] = $mdinfo["dirname"];
-			foreach ($configarray as $index => $configitem) {
-				$this->Configuration[$index] = $configitem;
 			}
 		}
 	}
