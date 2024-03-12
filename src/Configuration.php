379c6b0c3a596f8fadbe6177bf53991b3da18de2
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-03-12 03:49:44 +0000

    Update Configuration.php

--- src/Configuration.php
+++ src/Configuration.php
@@ -11,9 +11,7 @@
 			$this->Configuration = $this->DefaultConfiguration;
 			$mdinfo = pathinfo($configarray["doc_file"]);
 			$this->Configuration["doc_root"] = preg_replace('/( [\.][\/] )/', "", $mdinfo["dirname"]);
-			echo $this->Configuration["doc_root"] . "<br>";
 			$this->Configuration["doc_file"] = preg_replace('/( [\.][\/] )/', "", $mdinfo["basename"]);
-			echo $this->Configuration["doc_file"] . "<br>";
 			foreach ($configarray as $index => $configitem) {
 				if ($index != "doc_file" && $index != "doc_root") {
 					$this->Configuration[$index] = $configitem;
