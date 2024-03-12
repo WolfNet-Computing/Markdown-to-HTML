74b2a78bb3458c98da460a2d233820fd49c6164b
DarkestSoul1992 (John Wolfe) <65527568+DarkestSoul1992@users.noreply.github.com>
2024-03-12 01:45:47 +0000

    Update HtmlParser.php

--- src/HtmlParser.php
+++ src/HtmlParser.php
@@ -325,7 +325,7 @@
 			if (isset($this->Configuration["head_data"])) {
 				$counter = 0;
 				for ($i = count($this->Configuration["head_data"]) - 1; $i == 0; $i--) {
-					$this->InsertIntoArray($FormattedOutput, 0, $headitem);
+					$this->InsertIntoArray($FormattedOutput, 0, $this->Configuration["head_data"][$i]);
 				}
 			}
 			return $FormattedOutput;
